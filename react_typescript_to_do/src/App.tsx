1718fca7436f087ca8d7ced33e14bba7488a3e08
filmons <filmonseare@gmail.com>
2021-10-14 16:19:54 +0200

    Configuration development environment Taypescript in React 🚀🚀🚀

---
+++ react_typescript_to_do/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header >
+       <div className=""></div>
+       <div className=""></div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
