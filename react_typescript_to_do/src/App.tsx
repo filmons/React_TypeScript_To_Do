cb6aabee596561a533578835fb520bf56d5bacbc
filmons <filmonseare@gmail.com>
2021-10-16 19:19:33 +0200

    completed to do list on react and typescript 🚀🚀🚀🚀🚀

--- react_typescript_to_do/src/App.tsx
+++ react_typescript_to_do/src/App.tsx
@@ -1,10 +1,44 @@
-import React, {FC} from 'react';
+import React, {FC,ChangeEvent, useState} from 'react';
 import './App.css';
+import{ITask} from './Interfaces';
+import TodoTask from './Components/TodoTask'
 
 
 
 
 const App: FC  = ()  =>{
+ const [task, setTask] = useState<string>("");
+ const [deadline, setDeadline] = useState<number>(0);
+ const [todoList, setTodoList] = useState<ITask[]>([]);
+
+
+const handleChange = (event:ChangeEvent<HTMLInputElement>) :void => {
+  if (event.target.name === "task"){
+    setTask(event.target.value)
+  }else{
+
+    setDeadline( Number(event.target.value))
+  }
+  
+};
+const  addTask = (): void => {
+  const newTask = { taskName:task, deadline:deadline};
+  
+  setTodoList([...todoList, newTask]);
+  console.log(todoList)
+setTask("");
+setDeadline(0)
+
+
+};
+const completeTask = (taskNameToDeete : string):void => {
+setTodoList(todoList.filter((task) => {
+ return task.taskName !== taskNameToDeete
+}) )
+  
+}
+
+
   return (
     <div className="App">
       <div className="header">
@@ -13,22 +47,29 @@ const App: FC  = ()  =>{
             type="text"
             placeholder="Task..."
             name="task"
+            value={task}
+            onChange={handleChange}
             
           />
           <input
             type="number"
             placeholder="Deadline (in Days)..."
             name="deadline"
+            value={deadline}
+            onChange={handleChange}
             
             
           />
         </div>
-        <button >Add Task</button>
+        <button onClick={addTask} >Add Task</button>
       </div>
       <div className="todoList">
         {/* {todoList.map((task: ITask, key: number) => {
           return <TodoTask key={key} task={task} completeTask={completeTask} />;
         })} */}
+        {todoList.map((task: ITask, key: number) => { 
+        return<TodoTask key={key} task={task} completeTask={completeTask} />
+        })}
       </div>
     </div>
   );
---
+++ react_typescript_to_do/src/Components/TodoTask.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ITask } from '../Interfaces'
+interface props {
+    task :ITask;
+    completeTask(taskNameToDelet: string): void;
+}
+
+const TodoTask = ({task, completeTask}: props) => {
+    return(
+
+        <div className="task">
+            <div className="content">
+            <span> {task.taskName}</span>
+            <span>  {task.deadline}</span>
+            
+            </div>
+            <button onClick={()=> {
+                completeTask(task.taskName)
+            }} >
+                x</button>
+         </div>
+    )
+   
+}
+
+export default TodoTask
